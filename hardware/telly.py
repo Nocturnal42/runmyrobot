2e841f94c639fd664166e61ab9fe86a0d825a66e
Peter Steele <psteele@veraida.com>
2017-12-23 17:46:41 +1100

    Removed the now unneeded code for read the conf file for tellys.

--- hardware/telly.py
+++ hardware/telly.py
@@ -4,41 +4,12 @@ import extended_command
 module = None
 ser = None
 
-right_wheel_forward_speed = None
-right_wheel_backward_speed = None
-left_wheel_forward_speed = None
-left_wheel_backward_speed = None
-turn_delay = None
-straight_delay = None
-led_max_brightness = None
 
 
 def setup(robot_config):
     global module
     global ser
-    global straight_delay
-    global turn_delay
-    global right_wheel_forward_speed
-    global right_wheel_backward_speed
-    global left_wheel_forward_speed
-    global right_wheel_backward_speed
-    global led_max_brightness
-    
-    led_max_brightness = robot_config.get('telly', 'led-max-brightness')
-    
-    if robot_config.has_option('telly', 'right_wheel_forward_speed'):
-        right_wheel_forward_speed = robot_config.get('telly', 'right_wheel_forward_speed')
-    if robot_config.has_option('telly', 'right_wheel_backward_speed'):
-        right_wheel_forward_speed = robot_config.get('telly', 'right_wheel_backward_speed')
-    if robot_config.has_option('telly', 'left_wheel_forward_speed'):
-        left_wheel_forward_speed = robot_config.get('telly', 'left_wheel_forward_speed')
-    if robot_config.has_option('telly', 'left_wheel_backward_speed'):
-        right_wheel_forward_speed = robot_config.get('telly', 'left_wheel_backward_speed')
-
-    
-    straight_delay = robot_config.getfloat('robot', 'straight_delay')
-    turn_delay = robot_config.getfloat('robot', 'turn_delay')    
-    
+       
     module = mod_utils.import_module('hardware', 'serial_board')
     ser = module.setup(robot_config)
 
